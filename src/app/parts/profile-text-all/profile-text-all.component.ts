f30c1d603397dd1431916e28302c9f3b7ba7bd60
鈴木　泰斗 <taisuzuk@MBP-15JAH-036.local>
2019-03-07 17:21:50 +0900

    Add remote repo

--- src/app/parts/profile-text-all/profile-text-all.component.ts
+++ src/app/parts/profile-text-all/profile-text-all.component.ts
@@ -1,21 +1,64 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-text-all',
   templateUrl: './profile-text-all.component.html',
   styleUrls: ['./profile-text-all.component.scss']
 })
-export class ProfileTextAllComponent implements OnInit {
+export class ProfileTextAllComponent implements OnInit, AfterViewInit {
 
   @Input()
   public width: string;
 
   @Input()
-  public height: string;
+  public linesDescription: number;
 
-  constructor() { }
+  @Input()
+  public agentName: string;
+
+  @Input()
+  public agentDescription: string;
+
+  @Input()
+  public nameOnly: boolean;
+
+  @Input()
+  public displayAvatar: boolean;
+
+  @ViewChild('description')
+  public elemDescription: ElementRef;
+
+  constructor() {
+    this.linesDescription = null;
+    this.nameOnly = false;
+    this.displayAvatar = true;
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.resetDescriptionHeight();
+  }
+
+  private resetDescriptionHeight(): void {
+    if (this.nameOnly) {
+      return;
+    }
+    if (!this.linesDescription) {
+      return;
+    }
+    if (this.linesDescription <= 0) {
+      return;
+    }
+    const d: HTMLDivElement = this.elemDescription.nativeElement;
+    const s: CSSStyleDeclaration = window.getComputedStyle(d);
+    const preLineHeight = parseInt(s.height, 10);
+    const afterLineHeight = parseInt(s.lineHeight, 10);
+    if (preLineHeight < afterLineHeight) {
+      return;
+    }
+    d.style.height = `${afterLineHeight * this.linesDescription}px`;
   }
 
 }
