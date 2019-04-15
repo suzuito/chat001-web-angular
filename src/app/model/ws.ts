f370cf7d030b6d6a93b311094ca78704063bd091
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2019-04-16 00:03:28 +0900

    save

--- src/app/model/ws.ts
+++ src/app/model/ws.ts
@@ -1,7 +1,18 @@
+import { EasyAgent, RoomAgentIn } from './agent';
+import { Message } from './room_message';
+import { AgentInRoom } from './room';
 
 // WSMessage ...
 export interface WSMessage {
   type: string;
   id: string;
   data: any;
+}
+
+// WSRoomMessage ...
+export interface WSRoomMessage {
+  readonly roomAgentIds: string[];
+  readonly message: Message;
+  readonly agent: AgentInRoom;
+  readonly room: RoomAgentIn;
 }
